b449d33764cdf77bd03edfcdcd09ba65a8d1d4a9
zianad <infinitydecosign@gmail.com>
2025-09-25 19:13:29 +0100

    Final configuration fix

--- index.tsx
+++ index.tsx
@@ -5,9 +5,5 @@ import App from './App';
 const container = document.getElementById('root');
 if (container) {
   const root = createRoot(container);
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
+  root.render(<App />);
 }
