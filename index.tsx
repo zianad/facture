02c23fb71551b3a2839af8ed080e23084bc00a80
zianad <infinitydecosign@gmail.com>
2025-09-25 20:04:21 +0100

    Final configuration fix

--- index.tsx
+++ index.tsx
@@ -1,13 +1,13 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import React from 'react';
+import * as ReactDOM from 'react-dom/client';
 import App from './App';
 
 const container = document.getElementById('root');
 if (container) {
-  const root = createRoot(container);
+  const root = ReactDOM.createRoot(container);
   root.render(
-    <StrictMode>
+    <React.StrictMode>
       <App />
-    </StrictMode>
+    </React.StrictMode>
   );
 }
