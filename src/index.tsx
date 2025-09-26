e209884044b46aa171a0a124c1459cfd2fb56d73
zianad <infinitydecosign@gmail.com>
2025-09-26 17:00:08 +0100

    feat: Final radical restructure and config reset

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import * as ReactDOM from 'react-dom/client';
+import App from '@/App';
 
 const container = document.getElementById('root');
 if (container) {
